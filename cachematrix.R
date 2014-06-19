a5842dc84f474b2c7694012f3b48512de56a8ec1
adamquinn <aquinn@gmail.com>
2014-06-18 22:48:43 -0400

    added comments to functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+#makeCacheMatrix() creates a cacheable "matrix" object.  Function contains 4 main functions:
+#set, get, setmatrix, getmatrix
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+  
+  m <- NULL
+  
+  # set function will create the matrix
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  #get function returns the created matrix
+  get <- function()x
+  setmatrix <- function(matrix) m<<- matrix
+  getmatrix <- function()m
+  
+  #list of functions that can be utilized.  x$set and x$get create and return matrix respectively
+  list(set = set, get = get, setmatrix = setmatrix, getmatrix = getmatrix)
+  
 }
 
 
-## Write a short comment describing this function
+
+#cacheSolve returns the inverse of the matrix returned by the "get" function in makeCacheMatrix()
+#If the inverse has already been calculated the cachesolve will retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  m <- x$getmatrix()
+  
+  #logic checks to see if the matrix is already cached.  If so, a message is printed and the 
+  #solve function is performed on the object.  The object is then returned to the calling function
+  if(!is.null(m)) {
+    message("data is cached")
+    solve(m)
+    return(m)
+  }
+  
+  #if matrix is not already cached, the solve function is performed and the object is cached using
+  #the setmatrix() operation from the makeCacheMatrix() function.
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmatrix(m)
+  m
 }
